04476aa308de1956fad222526cfd1700c07b65aa
Zero_X <rvkov536@gmail.com>
2023-07-19 12:07:36 -0400

    completed tag routes js file

--- Assets/routes/api/tag-routes.js
+++ Assets/routes/api/tag-routes.js
@@ -26,16 +26,44 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(500).json({ message: 'failed to create category' });
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = Tag.update(req.body, {
+      where: { id: req.params.id }
+    });
+    res.status(200).json(updateTag);
+  } catch (err) {
+    res.status(404).json({ message: 'failed to find Tag by this id'})
+    res.status(500), json({ message: 'failed to update category by id' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagId = req.params.id;
+    const deleteTag = await Product.findByPk(tagId);
+
+    if (!deleteTag) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    await tagId.destroy();
+
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to delete product by id' });
+  }
 });
 
 module.exports = router;
